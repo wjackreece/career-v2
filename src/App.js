efd931baad96469ce93c30206a1343bc5dc639cf
Wynstona Jackreece <wjackreece@gmail.com>
2023-06-28 14:58:51 -0400

    Added Route to Welcome component on App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
+import Welcome from "./components/Welcome/Welcome";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" exact element={<Home />} />
+        <Route path="/welcome" exact element={<Welcome />} />
       </Routes>
     </BrowserRouter>
   );
