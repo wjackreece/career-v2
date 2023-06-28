7b9e27de5f96794e0a0b8c7a3c62e947618f8e21
Wynstona Jackreece <wjackreece@gmail.com>
2023-06-27 20:46:01 -0400

    Removed icon on Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -28,7 +28,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            ReWork <i className="fab fa-typo3"></i>
+            ReWork
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
