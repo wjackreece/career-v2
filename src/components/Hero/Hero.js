16e02617015bdd7ef0009fac09dc4fd31e2f6234
Wynstona Jackreece <wjackreece@gmail.com>
2023-07-01 22:28:03 -0400

    Add JS and CSS to Welcome component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../../App.css";
-import { Button } from "../Button/Button";
 
 import "./Hero.css";
 
@@ -17,16 +16,6 @@ function Hero() {
           A network with the skills, knowledge, and resources <br /> to support
           you through a successful career change{" "}
         </p>
-
-        <div className="hero-btns">
-          <Button
-            className="btns"
-            buttonStyle="btn--primary"
-            buttonSize="btn--large"
-          >
-            Let's Get Started
-          </Button>
-        </div>
       </div>
     </div>
   );
