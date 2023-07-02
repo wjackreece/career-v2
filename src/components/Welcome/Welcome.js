1845733f77295c64ede4084bc64d0462696a54c5
Wynstona Jackreece <wjackreece@gmail.com>
2023-07-02 16:55:14 -0400

    Styled media for Welcome component

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -8,22 +8,22 @@ function Welcome() {
       <h1 className="welcome-title">Welcome to ReWork!</h1>
       <div className="expectation-container">
         <h2>Here's what to expect:</h2>
-        <div>
-          <div>
+        <div className="expectation-info-container">
+          <div className="numbers-container">
             <div className="numbers">1</div>
             <div className="number-desc">
-              Tell us about your career change aspirations so we can customize
-              ReWork to your situation.
+              Tell us about your current career and desired career aspirations
+              so we can customize ReWork to your situations.
             </div>
           </div>
-          <div>
+          <div className="numbers-container">
             <div className="numbers">2</div>
             <div className="number-desc">
               Based on your answers, we'll show you what characteristcs you
               already share with career fields within your desired scope.
             </div>
           </div>
-          <div>
+          <div className="numbers-container">
             <div className="numbers">3</div>
             <div className="number-desc">
               Chose a specific career path, and we'll provide education
