ffb7a98c21930ad29a1e0fd6fb0fd5a2f09fbdd3
Wynstona Jackreece <wjackreece@gmail.com>
2023-07-08 16:19:01 -0400

    Add ChatGPT to Navbar and App.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import CareerChangeSelector from "./components/CareerChangeSelector/CareerChangeSelector";
 import LoadingPage from "./components/LoadingPage/LoadingPage";
+import ChatGPTInfo from "./components/ChatGPTInfo/ChatGPTInfo";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<Home />} />
         <Route path="/job-comparisons" element={<CareerChangeSelector />} />
         <Route path="/loading-page" element={<LoadingPage />} />
+        <Route path="/chatGPTInfo" element={<ChatGPTInfo />} />
+        <Route path="/" exact element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
